e03770a379439975a6d5048ba2f7ad286c3a9a6a
Marco220603 <217613265+MarcoFerrey@users.noreply.github.com>
2025-08-23 03:24:41 -0500

    login corregido

--- login/urls.py
+++ login/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from login import views
 
 urlpatterns = [
-    path('', views.sign_in, name="signin")
+    path('', views.sign_in, name="signin"),
+    path('/login', views.sign_in, name="signin")
 ]
